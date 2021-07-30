5f2d2273a3004eaea2cfee6fb1ca766fd3329123
011107 <87961752+011107@users.noreply.github.com>
2021-07-30 12:24:23 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw() 
+{
+  background(30);
+  if(keyDown("right")) {
+    background("red")
+  }
+
+  if(keyDown("left")) {
+    background("blue")
+  }
+  if(keyDown("up")) {
+    background("yellow")
+  }
+  if(keyDown("down")) {
+    background("green")
+  }
+
+}
+
+
+
+
